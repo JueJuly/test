8f76f86a4efdf873f317ad1a89685fd24c3c5ffb
JueJuly <zhuzhilei30@163.com>
2015-11-22 15:43:54 +0800

    add test-compare function

--- compare.c
+++ compare.c
@@ -1,4 +1,28 @@
 #include <math.h>
 
 #define Max(a,b) ((a)>(b) ? (a) : (b))
+
 #define Min(a,b) ((a)<(b) ? (a) : (b))
+
+bool test_compare(int a, int b )
+{
+  if( a > b ){
+
+    if( a == Max(a,b) ){
+      return true;
+    }else{
+      return false;
+    }
+
+  }else{
+
+    if( a == Min(a,b) ){
+      return true;
+    }else{
+     return false;
+     
+    }
+
+  }
+
+}
